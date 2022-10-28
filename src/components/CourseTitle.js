fff781176ad811eb5fdbd7db4c65a4d915e5df99
Taher027 <abutaherh27@gmail.com>
2022-10-28 20:21:18 +0600

    added layout design

--- src/components/CourseTitle.js
+++ src/components/CourseTitle.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 const CourseTitle = () => {
 
     const [course, setCourse] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:5000/courses')
-        .then(res => res.json())
-        .then(data=>setCourse(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setCourse(data))
+    }, [])
 
     return (
         <div>
@@ -17,11 +19,17 @@ const CourseTitle = () => {
             <div>
                 {
                     course.map(c => <p key={course.id}>
-                        
+
                         <Link to={`course/${c.id}`}>{c.title}</Link>
 
                     </p>)
                 }
+            </div>
+            <div>
+                <ButtonGroup vertical>
+                    <Button variant='outline-primary'> <FaGoogle></FaGoogle> Login with Google</Button>
+                    <Button variant='outline-dark'><FaGithub></FaGithub> Login with Github</Button>
+                </ButtonGroup>
             </div>
         </div>
     );
