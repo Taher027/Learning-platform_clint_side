5cec55cfa6780121ca4ad20ce0d2e9197bbc42d2
Taher027 <abutaherh27@gmail.com>
2022-10-30 10:29:34 +0600

    error page

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+             <div className="text-center text-info">
+      <h1>404!</h1>
+      <h2>Page not found!!!!</h2>
+    </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
